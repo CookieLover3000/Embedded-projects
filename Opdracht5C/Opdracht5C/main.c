2d6abc70e8ddd321c4c11cc1667c68ef0d558ea6
CookieLover3000 <77576646+CookieLover3000@users.noreply.github.com>
2022-03-23 13:23:29 +0100

    Opdrach5A en 5B af

---
+++ Opdracht5C/Opdracht5C/main.c
@@ -0,0 +1,18 @@
+/*
+ * Opdracht5C.c
+ *
+ * Created: 23-3-2022 13:16:44
+ * Author : iwanv
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
