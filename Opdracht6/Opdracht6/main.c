9a78e20dc6b7086c76639eae2ed0a8ec52904652
CookieLover3000 <77576646+CookieLover3000@users.noreply.github.com>
2022-03-31 20:56:28 +0200

    OPDRACHT 6 AF

--- Opdracht6/Opdracht6/main.c
+++ Opdracht6/Opdracht6/main.c
@@ -1,12 +1,14 @@
 /*
- * Opdracht6ABC.c
+ * Opdracht6.c
  *
  * Created: 30-3-2022 09:56:29
  * Author : iwanv
  */ 
 #define F_CPU 16000000
+#define TOP16bit 65535 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 void initADC();
 void initTimer2FastPWM();
 void initTimer1Fast10PWM();
@@ -18,12 +20,15 @@ uint16_t leesADwaarde(uint8_t);
 
 int main(void)
 {
+	/* Deel C */
 //	uint8_t periodetijden[] = {1,2,3,4,5}; // prescalers
+//	initTimer1Fast10PWM();
+	/* Niet meer deel C*/
 	initTimer1FastPWM_ICR1();	// initialiseer timer1 op fast PWM
 	initTimer2FastPWM();	// initialiseer timer2 op fast PWM
 	initADC();
 //	_delay_ms(1000);		// wacht 1 seconde
-//	dimLed(255);			// Duty cycle van PWM = 1
+//	dimLed(50);			//De duty cycle van PM is 50/256 * 100 = 19.5
 //	_delay_ms(1000);		// wacht 1 seconde
 	
     while (1) 
@@ -31,11 +36,15 @@ int main(void)
 		long adwaarde=leesADwaarde(5); //long i.v.m omrekenen
 		adwaarde=adwaarde*255/1023;
 		dimLed(adwaarde);
-//		for(int i=0;i<5;i++)
-//		{
-//			pasPeriodeTijdTimer1Aan(periodetijden[i]); // functie past de frequentie aan f=1/periodetijd
-//			_delay_ms(1000);
-//		}
+	/*--------*/
+	/* Deel C */
+	/*--------*/
+/*		for(int i=0;i<5;i++)
+		{
+			pasPeriodeTijdTimer1Aan(periodetijden[i]); // functie past de frequentie aan f=1/periodetijd
+			_delay_ms(1000);
+		}
+*/
     }
 	return 0;
 }
@@ -47,7 +56,7 @@ void initTimer2FastPWM()
 	TCCR2B = (1 << CS20); // prescaler
 }
 
-void initTimer1Fast10PWM()
+void initTimer1Fast10PWM() // F_CPU / 64 * (1+1024) voor 244hz
 {
 	DDRB = (1 << PORTB1);
 	TCCR1A = (1 << COM1A1) | (1 << WGM10) | (1 << WGM11);
@@ -55,13 +64,15 @@ void initTimer1Fast10PWM()
 	OCR1A = 512;
 }
 
-void initTimer1FastPWM_ICR1()
+void initTimer1FastPWM_ICR1() // mode 14 for ICR top value... 2 uur hiernaar op zoek geweest fml
 {
 		DDRB = (1 << PORTB1);
-		TCCR1A = (1 << COM1A1) | (1 << WGM10) | (1 << WGM11);
-		TCCR1B = (1 << CS10)| (1 <<CS11) |(1 << WGM12);
-		OCR1A = 512;
-		ICR1 = 1023;
+		TCCR1A = (1 << COM1A1) | (1 << WGM11);
+		TCCR1B = (1 << CS10) | (1 << CS12) | (1 << WGM12) | (1 << WGM13); // prescaler 1024
+		OCR1A = (TOP16bit/2); // 50 % duty cycle
+		ICR1 = TOP16bit; // max value of ICR1
+		TIMSK1 = (1 << ICIE1) | (1 << OCIE1A);
+		sei();
 }
 
 void initADC()
@@ -92,4 +103,15 @@ void pasPeriodeTijdTimer1Aan(uint8_t prescalers)
 {
 	TCCR1B &= 0x10;
 	TCCR1B |= prescalers;
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	TCCR2A &= ~(1 << WGM20) & ~(1 << WGM21); // PWM op OC2B uit
+	PORTD &= ~(1 << PORTD3); // zet PORTD3 laag
+}
+
+ISR(TIMER1_CAPT_vect)
+{
+	TCCR2A |= (1 << WGM20) | (1 << WGM21);
 }
