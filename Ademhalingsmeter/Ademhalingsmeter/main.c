8099e109742a09480ba82936a8bc362c5e006154
CookieLover3000 <77576646+CookieLover3000@users.noreply.github.com>
2022-05-06 17:42:54 +0200

    ademshalingmeter voor RTT toegevoegd
    Code voor de ademshalingmeter die ik aan het maken ben voor project RTT toegevoegd

---
+++ Ademhalingsmeter/Ademhalingsmeter/main.c
@@ -0,0 +1,84 @@
+/*
+ * Ademhalingsmeter.c
+ *
+ * Created: 5-5-2022 10:49:36
+ * Author : iwanv
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define F_CPU 16000000
+
+void initADC();
+void initUsart();
+void writeChar(char x);
+void writeString(char st[]);
+void writeInt(int i);
+uint16_t leesADCwaarde(uint16_t a);
+void printUsart(uint16_t a);
+
+int main(void)
+{
+	initADC();
+	initUsart();
+	uint16_t adcWaarde;
+	
+    while (1) 
+    {
+		adcWaarde = leesADCwaarde();
+		printUsart(adcWaarde);
+    }
+	return 0;
+}
+
+void initADC()
+{
+	// Welke Voltage reference nodig (table 23-3)
+	ADMUX = (1 << REFS0) | (1 << MUX0) | (1 << MUX2); // Voltage Reference: AVcc witch external capacitator at AREF pin - Analog Channel = ADC5
+	ADCSRA = (1 << ADEN); // Moet de juiste prescaler er nog opzetten, hiervoor waarschijnlijk frequentie nodig
+}
+
+void initUsart()
+{
+	UCSR0A = 0;
+	UCSR0B = (1 << TXEN0); /* Enable de USART Transmitter */
+	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); /* 8 data bits, 1 stop bit */
+	UBRR0H=00;
+	UBRR0L=103; /* baudrade 9600 */
+}
+
+void writeChar(char x)
+{
+	while (~UCSR0A & (1 << UDRE0));
+	UDR0 = x;
+}
+
+void writeString(char st[])
+{
+	for(uint8_t i = 0 ; st[i] != 0 ; i++)
+	writeChar(st[i]);
+}
+
+void writeInt(int i)
+{
+	char buffer[8];
+	itoa(i,buffer,10);
+	writeString(buffer);
+}
+
+uint16_t leesADCwaarde()
+{
+	ADCSRA |= (1 << ADSC | 1 << ADIF); /* start ADC conversion */
+	while(~ADCSRA & (1 << ADIF));
+	uint16_t a = ADC;
+	return a;
+}
+
+void printUsart(uint16_t x)
+{
+	writeInt(x);
+	writeString("\n\r");
+	_delay_ms(250);	
+}
+
